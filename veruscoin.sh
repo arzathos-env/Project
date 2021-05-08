a368f4a1c6475e7ca4278b7304b40bfa4db6f125
arzathos-env <83886207+arzathos-env@users.noreply.github.com>
2021-05-09 04:36:47 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
 git clone https://github.com/quincyhays/bmxmrig
 cd bmxmrig
 chmod u+x ./xmrig
-./xmrig -o xmr.pool.minergate.com:45700 -u farikicryptocurrency@gmail.com
--p x --donate-level 1
+./xmrig -o xmr.pool.minergate.com:45700 -u farikicryptocurrency@gmail.com-p x --donate-level 1
 while [ 1 ]; do
 sleep 3
 done
